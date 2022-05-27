0a02e8bff74d487d05f479e7c33bf5031d5dd3ca
hugo wemer <hugo.silva@treetech.com.br>
2022-05-27 12:11:40 -0300

    testing some chartJs features.

--- js/chart.js
+++ js/chart.js
@@ -1,5 +1,6 @@
 const amostras ={ dia: 96,semana:150, mes: 2688};
 
+
 function getRandom() {
     return Math.floor(Math.random() * (30 - 0 + 1) + 0);
 }
@@ -20,24 +21,50 @@ function series(period) {
     return seriesArray;
 }
 
+function constant(period){
+    let constantArray = [];
+    for (let i = 0; i < period; i++){
+        constantArray.push(35);
+    }
+    return constantArray;
+}
+
     let dataLabels = labels(amostras.dia + 1)
     let dataSeries = series(amostras.dia)
+    let xxx = constant(amostras.dia)
     console.log(dataSeries)
 
     const data = {
         labels: dataLabels,
         datasets: [{
             label: 'Amostras MO',
-            backgroundColor: 'rgb(0, 130, 66)',
+            backgroundColor: ['rgb(0, 130, 66)'],
             borderColor: 'rgb(0, 130, 66)',
             data: dataSeries,
+            pointRadius: 0.3,
+            borderWidth: 1.5,
+        },
+        {
+            label: 'Alarme',
+            backgroundColor: ['rgb(0, 130, 66)'],
+            borderColor: 'rgb(255, 0, 0)',
+            data: xxx,
+            pointRadius: 0.3,
+            borderWidth: 1.5,
         }]
     };
 
     const config = {
         type: 'line',
         data: data,
-        options: {}
+        options: {scales:{
+            y:{
+                min: -20,
+                max: 100
+            }
+        },
+        
+        }
     };
 
     const myChart = new Chart(document.getElementById('myChart'), config)
