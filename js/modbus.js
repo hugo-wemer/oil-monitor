fe08187262ea38f917fee865cd63602b86763127
hugo wemer <hugo.silva@treetech.com.br>
2022-05-30 16:10:10 -0300

    Setting up modbus serial reader master

--- js/modbus.js
+++ js/modbus.js
@@ -1,12 +1,17 @@
-var ModbusRTU = require("modbus-serial");
-var client = new ModbusRTU();
+const ModbusRTU = require("modbus-serial");
+const client = new ModbusRTU();
+// open connection to a serial port
+client.setID(1);
 
-client.connectRTUBuffered("/dev/ttyUSB0", { baudRate: 9600, parity: 'none' });
+client.connectRTUBuffered("/dev/ttyUSB0", { baudRate: 9600 });
 
+let arrayData = [];
+setInterval(function(){
+    client.readHoldingRegisters(5, 1, function(err, data){
+        arrayData.push(data.data[0])
+        console.log(arrayData);
+    })
 
-client.setID(2);
+},2000)
 
-function read(){
-  client.readHoldingRegisters(0, 1)
-    .then(console.log);
-}
+//console.log(read.value)
