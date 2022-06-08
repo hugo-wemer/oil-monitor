5c3d305fc6d4a74cb4c74dbf82d00e85b7df1fad
hugo wemer <hugo.silva@treetech.com.br>
2022-06-07 23:14:37 -0300

    build: salvar dados obtidos via protocolo em um json

--- service/modbus.js
+++ service/modbus.js
@@ -2,13 +2,11 @@ const fs = require('fs');
 const ModbusRTU = require("modbus-serial");
 const client = new ModbusRTU();
 client.setID(5);
-client.connectRTUBuffered("COM6", { baudRate: 9600 });
+client.connectRTUBuffered("/dev/ttyUSB1", { baudRate: 9600 });
 
 const db = './log.json'
 const interval = 0.05; //intervalo entre amostras (minutos)
-// let paramsArray = ["rsa", "rsma", "h2oa", "h2oma", "tha"]
-// let alarmsArray = ["rsa", "rsma", "h2oa", "h2oma", "trend", "selfdiag"]
-// let measurementArray = ["rs", "rstr", "h2o", "temp", "trend", "temp2"]
+
 
 setInterval(async function(){
     let indications = [];
@@ -18,10 +16,15 @@ setInterval(async function(){
     params.forEach(resultParams => {                        // resultParams = {data:[], buffer:<>}
         fs.readFile(db, 'utf8', (err, currentJSON) => {
             let dbData = JSON.parse(currentJSON)
+            dbData[0].parameters.rsa.splice(0, 1)
             dbData[0].parameters.rsa.push(resultParams.data[0])
+            dbData[0].parameters.rsma.splice(0, 1)
             dbData[0].parameters.rsma.push(resultParams.data[1])
+            dbData[0].parameters.h2oa.splice(0, 1)
             dbData[0].parameters.h2oa.push(resultParams.data[2])
+            dbData[0].parameters.h2oma.splice(0, 1)
             dbData[0].parameters.h2oma.push(resultParams.data[3])
+            dbData[0].parameters.tha.splice(0, 1)
             dbData[0].parameters.tha.push(resultParams.data[4])
             fs.writeFile(db, JSON.stringify(dbData) , (err, data) => {})
         })
@@ -37,27 +40,30 @@ setInterval(async function(){
             dbData[0].indications.measurements.trend.push(resultIndications.data[5])
             dbData[0].indications.measurements.temp2.push(resultIndications.data[6])
 
-            // var alarms = resultIndications.data[10]
-            // if(alarms == 0){
-            //     dbData[0].indications.alarms.rsa.push(0)
-            //     dbData[0].indications.alarms.rsma.push(0)
-            //     dbData[0].indications.alarms.h2oa.push(0)
-            //     dbData[0].indications.alarms.h2oma.push(0)
-            //     dbData[0].indications.alarms.trend.push(0)
-            //     dbData[0].indications.alarms.selfdiag.push(0)
+            if (resultIndications.data[7] >= 4){    // maior ou igual a quatro pois é o valor que representa o relé de autodiag (0000100 = 4)
+                dbData[0].indications.alarms.selfdiag.splice(0, 1)
+                dbData[0].indications.alarms.selfdiag.push(1)
+            }
 
 
-            // } else if(){
 
-            //}
-            
-            
-            // dbData[0].indications.alarms.rsa.push()
-            // dbData[0].indications.alarms.rsma.push()
-            // dbData[0].indications.alarms.h2oa.push()
-            // dbData[0].indications.alarms.h2oma.push()
-            // dbData[0].indications.alarms.trend.push()
-            // dbData[0].indications.alarms.selfdiag.push()
+            let alarms = resultIndications.data[8]
+            alarms = alarms.toString(2)
+            alarms = alarms.split('')
+            let alarmItems = ["rsa", "rsma", "h2oa", "h2oma", "trend"]
+            let i = alarms.length;
+            alarmItems.forEach(item => {
+                console.log(dbData[0].indications.alarms.item)  // A vida seria tão mais fácil se esse item funcionasse assim...
+                // if(alarms[i] == true){
+                //     dbData[0].indications.alarms.item.splice(0, 1);
+                //     dbData[0].indications.alarms.item.push(1);
+                // }else{
+                //     dbData[0].indications.alarms.item.splice(0, 1)
+                //     dbData[0].indications.alarms.item.push(0)
+                // }
+                i--
+            })
+
             fs.writeFile(db, JSON.stringify(dbData) , (err, data) => {})
         })
     })
